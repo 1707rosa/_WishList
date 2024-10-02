63837562e696e5ce197da76ceff79e3d8be6c866
1707rosa <160504503+1707rosa@users.noreply.github.com>
2024-10-02 13:16:19 -0700

    eliminando-proyecto

--- src/DesireBoard/DesireBoard.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DesireBoard.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
